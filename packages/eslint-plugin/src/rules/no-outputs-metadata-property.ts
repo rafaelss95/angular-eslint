b9eff23d718cab58969eca63c3bc81a731a079b3
Rafael Santana <rafaelsantana3095@gmail.com>
2020-12-23 12:41:39 -0300

    refactor(eslint-plugin): no-outputs-metadata-property switch to selectors

--- packages/eslint-plugin/src/rules/no-outputs-metadata-property.ts
+++ packages/eslint-plugin/src/rules/no-outputs-metadata-property.ts
@@ -1,15 +1,10 @@
 import { TSESTree } from '@typescript-eslint/experimental-utils';
 import { createESLintRule } from '../utils/create-eslint-rule';
 import { COMPONENT_OR_DIRECTIVE_CLASS_DECORATOR } from '../utils/selectors';
-import {
-  AngularInnerClassDecorators,
-  getDecoratorPropertyValue,
-} from '../utils/utils';
 
 type Options = [];
 export type MessageIds = 'noOutputsMetadataProperty';
 export const RULE_NAME = 'no-outputs-metadata-property';
-
 const METADATA_PROPERTY_NAME = 'outputs';
 const STYLE_GUIDE_LINK = 'https://angular.io/styleguide#style-05-12';
 
@@ -24,23 +19,17 @@ export default createESLintRule<Options, MessageIds>({
     },
     schema: [],
     messages: {
-      noOutputsMetadataProperty: `Use @${AngularInnerClassDecorators.Output} rather than the \`${METADATA_PROPERTY_NAME}\` metadata property (${STYLE_GUIDE_LINK})`,
+      noOutputsMetadataProperty: `Use @Output rather than the \`${METADATA_PROPERTY_NAME}\` metadata property (${STYLE_GUIDE_LINK})`,
     },
   },
   defaultOptions: [],
   create(context) {
     return {
-      [COMPONENT_OR_DIRECTIVE_CLASS_DECORATOR](node: TSESTree.Decorator) {
-        const propertyExpression = getDecoratorPropertyValue(
-          node,
-          METADATA_PROPERTY_NAME,
-        );
-        if (!propertyExpression) {
-          return;
-        }
-
+      [`${COMPONENT_OR_DIRECTIVE_CLASS_DECORATOR} Identifier[name="${METADATA_PROPERTY_NAME}"]`]({
+        parent,
+      }: TSESTree.Identifier) {
         context.report({
-          node: propertyExpression.parent as TSESTree.Property,
+          node: parent!,
           messageId: 'noOutputsMetadataProperty',
         });
       },
