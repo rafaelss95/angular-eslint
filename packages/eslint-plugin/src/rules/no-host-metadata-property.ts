0e4db915fc4f339b89d6489e02987b48d7a663c2
Rafael Santana <rafaelsantana3095@gmail.com>
2020-12-23 12:45:57 -0300

    refactor(eslint-plugin): no-host-metadata-property switch to selectors

--- packages/eslint-plugin/src/rules/no-host-metadata-property.ts
+++ packages/eslint-plugin/src/rules/no-host-metadata-property.ts
@@ -1,15 +1,10 @@
 import { TSESTree } from '@typescript-eslint/experimental-utils';
 import { createESLintRule } from '../utils/create-eslint-rule';
 import { COMPONENT_OR_DIRECTIVE_CLASS_DECORATOR } from '../utils/selectors';
-import {
-  AngularInnerClassDecorators,
-  getDecoratorPropertyValue,
-} from '../utils/utils';
 
 type Options = [];
 export type MessageIds = 'noHostMetadataProperty';
 export const RULE_NAME = 'no-host-metadata-property';
-
 const METADATA_PROPERTY_NAME = 'host';
 const STYLE_GUIDE_LINK = 'https://angular.io/styleguide#style-06-03';
 
@@ -24,23 +19,17 @@ export default createESLintRule<Options, MessageIds>({
     },
     schema: [],
     messages: {
-      noHostMetadataProperty: `Use @${AngularInnerClassDecorators.HostBinding} or @${AngularInnerClassDecorators.HostListener} rather than the \`${METADATA_PROPERTY_NAME}\` metadata property (${STYLE_GUIDE_LINK})`,
+      noHostMetadataProperty: `Use @HostBinding or @HostListener rather than the \`${METADATA_PROPERTY_NAME}\` metadata property (${STYLE_GUIDE_LINK})`,
     },
   },
   defaultOptions: [],
   create(context) {
     return {
-      [COMPONENT_OR_DIRECTIVE_CLASS_DECORATOR](node: TSESTree.Decorator) {
-        const propertyExpression = getDecoratorPropertyValue(
-          node,
-          METADATA_PROPERTY_NAME,
-        );
-        if (!propertyExpression) {
-          return;
-        }
-
+      [`${COMPONENT_OR_DIRECTIVE_CLASS_DECORATOR} Identifier[name="${METADATA_PROPERTY_NAME}"]`]({
+        parent,
+      }: TSESTree.Identifier) {
         context.report({
-          node: propertyExpression.parent as TSESTree.Property,
+          node: parent!,
           messageId: 'noHostMetadataProperty',
         });
       },
